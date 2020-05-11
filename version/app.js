d66b07394e18baaeaa785f852e2d59a9317344b0
Saiyesh Mahambrey <saiyeshm@us.ibm.com>
2020-05-11 15:19:37 +0530

    testing app update

--- version/app.js
+++ version/app.js
@@ -5,11 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-<<<<<<< HEAD
-    response = 'This is version 2 of the app.' + '\n';
-=======
-    response = 'This is version 3 of the app.' + '\n';
->>>>>>> 314fd50e61595fda6f9fbf547a6f5f9ca6173171
+    response = 'This is version 4 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
